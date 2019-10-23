e850474b6ff59de564ec8867eda111b07c033c99
Juniorsz <lequangvy812@gmail.com>
2019-10-23 12:24:02 +0700

    added postgres database configuration

--- config/database.php
+++ config/database.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Str;
 
+$DATABASE_URL = parse_url('postgres://etwpwysorlxeon:177becd67c17683de7061124d4185796955f7bb42e947280d9beb4b75fae6bfe@ec2-54-83-9-169.compute-1.amazonaws.com:5432/db8q3t0qd7ti2a');
+
 return [
 
     /*
@@ -65,12 +67,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'url' => 'postgres://etwpwysorlxeon:177becd67c17683de7061124d4185796955f7bb42e947280d9beb4b75fae6bfe@ec2-54-83-9-169.compute-1.amazonaws.com:5432/db8q3t0qd7ti2a',
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
