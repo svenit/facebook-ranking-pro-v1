7d179cbd6109edac998ea5240415439ccf3dfab8
Juniorsz <lequangvy812@gmail.com>
2019-12-16 23:25:53 +0700

    deloy heroku

--- config/app.php
+++ config/app.php
@@ -201,7 +201,6 @@ return [
         App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Barryvdh\Debugbar\ServiceProvider::class,
 
     ],
 
@@ -254,7 +253,6 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Socialite' => Laravel\Socialite\Facades\Socialite::class,
-        'Debugbar' => Barryvdh\Debugbar\Facade::class,
     ],
 
 ];
