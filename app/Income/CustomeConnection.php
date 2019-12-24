7e6ba8de24a5ef8b6532ba40eacb9bf15fd05ce5
Juniorsz <lequangvy812@gmail.com>
2019-12-24 18:20:44 +0700

    deloy to heroku

--- app/Income/CustomeConnection.php
+++ app/Income/CustomeConnection.php
@@ -10,7 +10,6 @@ class CustomeConnection
     {
         $connect = new PDO("pgsql:host=".env('DB_HOST').";dbname=".env('DB_DATABASE'),env('DB_USERNAME'),env('DB_PASSWORD'));
         $connect->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-        $connect->exec('SET NAMES UTF8MB4');
 
         $pusher = $connect->prepare("SELECT * FROM pushers WHERE selected = ?");
         $pusher->execute([1]);
